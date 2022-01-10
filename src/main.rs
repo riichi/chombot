44291dcaf0cc099d598cd4532a981ab68c13b0b6
Mateusz Maćkowski <mateusz@mackowski.org>
2022-01-10 21:16:47 +0100

    Add @everyone reactions, bot framework

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,98 @@
 use std::env;
 
+use serenity::{
+    async_trait,
+    model::{
+        gateway::Ready,
+        id::GuildId,
+        interactions::{
+            Interaction,
+            InteractionResponseType,
+        },
+    },
+    prelude::*,
+};
+use serenity::model::channel::Message;
+
 mod kcc3;
+
+const AT_EVERYONE_REACTIONS: [&'static str; 2] = ["Ichiangry", "Mikiknife"];
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, message: Message) {
+        if !message.mention_everyone {
+            return;
+        }
+
+        let emojis = message.guild_id.unwrap().emojis(&ctx.http).await.unwrap();
+        let emojis: Vec<_> = emojis.into_iter()
+            .filter(|x| AT_EVERYONE_REACTIONS.contains(&x.name.as_str()))
+            .collect();
+
+        for emoji in emojis {
+            message.react(&ctx, emoji).await.unwrap();
+        }
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let content = match command.data.name.as_str() {
+                "ping" => "Hey, I'm alive!".to_string(),
+                _ => "not implemented :(".to_string(),
+            };
+
+            if let Err(why) = command
+                .create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| message.content(content))
+                })
+                .await
+            {
+                println!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+
+        let guild_id = GuildId(
+            env::var("GUILD_ID")
+                .expect("Expected GUILD_ID in environment")
+                .parse()
+                .expect("GUILD_ID must be an integer"),
+        );
+
+        let _commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+            commands
+                .create_application_command(|command| {
+                    command.name("ping").description("A ping command")
+                })
+        })
+            .await;
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let kcc3_url = env::var("KCC3_URL").expect("Expected KCC3 URL in the environment");
-    let kcc3_token = env::var("KCC3_TOKEN").expect("Expected KCC3 token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    let kcc3client = kcc3::Kcc3Client::new(kcc3_url, &kcc3_token).unwrap();
-    println!("{:?}", kcc3client.get_chombos().await.unwrap());
+    let application_id: u64 = env::var("APPLICATION_ID")
+        .expect("Expected an application id in the environment")
+        .parse()
+        .expect("application id is not a valid id");
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
