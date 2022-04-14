b950924ce5583c209ffed446b3fb9b0b085b030f
Iipin <52832022+Iipin@users.noreply.github.com>
2022-04-14 16:38:09 +0200

    fix: address clippy warnings (#32)

--- src/slash_commands/utils.rs
+++ src/slash_commands/utils.rs
@@ -5,15 +5,12 @@ use serenity::model::interactions::application_command::{
 };
 use serenity::model::user::User;
 
-fn get_option<'a>(
-    options: &'a Vec<DataOption>,
-    option_name: &'static str,
-) -> Option<&'a DataOption> {
+fn get_option<'a>(options: &'a [DataOption], option_name: &'static str) -> Option<&'a DataOption> {
     options.iter().find(|option| option.name == option_name)
 }
 
 pub fn get_string_option<'a>(
-    options: &'a Vec<DataOption>,
+    options: &'a [DataOption],
     option_name: &'static str,
 ) -> Option<&'a str> {
     if let Some(option) = get_option(options, option_name) {
@@ -25,7 +22,7 @@ pub fn get_string_option<'a>(
 }
 
 pub fn get_user_option<'a>(
-    options: &'a Vec<DataOption>,
+    options: &'a [DataOption],
     option_name: &'static str,
 ) -> Option<(&'a User, &'a Option<PartialMember>)> {
     if let Some(option) = get_option(options, option_name) {
