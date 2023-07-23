72c5885876dc17912405f4b5419ebc62b568749c
Iipin <52832022+Iipin@users.noreply.github.com>
2023-07-23 22:59:52 +0200

    Add some tests

--- src/ranking_watcher/notifier.rs
+++ src/ranking_watcher/notifier.rs
@@ -8,15 +8,13 @@ use crate::ranking_watcher::usma::{PositionChangeInfo, Ranking};
 
 pub struct ChannelMessageNotifier {
     channel_id: ChannelId,
-    ctx: Context,
     message: String,
 }
 
 impl ChannelMessageNotifier {
-    pub fn new(channel_id: ChannelId, ctx: Context, message: String) -> Self {
+    pub fn new(channel_id: ChannelId, message: String) -> Self {
         Self {
             channel_id,
-            ctx,
             message,
         }
     }
@@ -64,13 +62,67 @@ impl ChannelMessageNotifier {
 
 #[async_trait]
 impl DataUpdateNotifier<Ranking> for ChannelMessageNotifier {
-    async fn notify(&self, _old_ranking: &Ranking, new_ranking: &Ranking) {
+    async fn notify(&self, _old_ranking: &Ranking, new_ranking: &Ranking, ctx: &Context) {
         let channel_id = self.channel_id;
-        let ctx = &self.ctx;
         let text = self.build_message(new_ranking);
 
         if let Err(why) = send_with_overflow(channel_id, ctx, text).await {
             error!("Could not send Ranking update: {why:?}");
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ranking_watcher::usma::RankingEntry;
+
+    #[test]
+    fn test_channel_message_notifier_build_message() {
+        let notifier = ChannelMessageNotifier::new(ChannelId(123), "TEST_MESSAGE".into());
+        let ranking = Ranking(vec![
+            RankingEntry {
+                pos: 1,
+                pos_diff: PositionChangeInfo::Diff(0),
+                name: "player-name-001".to_owned(),
+                points: 1966,
+                points_diff: PositionChangeInfo::Diff(0),
+            },
+            RankingEntry {
+                pos: 3,
+                pos_diff: PositionChangeInfo::Diff(3),
+                name: "player-name-002".to_owned(),
+                points: 1893,
+                points_diff: PositionChangeInfo::New,
+            },
+            RankingEntry {
+                pos: 5,
+                pos_diff: PositionChangeInfo::New,
+                name: "player-name-003".to_owned(),
+                points: 1830,
+                points_diff: PositionChangeInfo::Diff(-60),
+            },
+            RankingEntry {
+                pos: 8,
+                pos_diff: PositionChangeInfo::Diff(-3),
+                name: "player-name-004".to_owned(),
+                points: 1718,
+                points_diff: PositionChangeInfo::Diff(73),
+            },
+        ]);
+
+        let message = notifier.build_message(&ranking);
+
+        assert_eq!(
+            message,
+            concat!(
+                "TEST_MESSAGE\n",
+                "\n",
+                "Latest changes:\n",
+                "• 3 (↑3) / player-name-002 / 1893 pkt\n",
+                "• 5 (NEW) / player-name-003 / 1830 (-60) pkt\n",
+                "• 8 (↓3) / player-name-004 / 1718 (+73) pkt"
+            )
+        )
     }
 }
